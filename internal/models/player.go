4d392a6ffa32a632562e66304962238e70b1cfe9
prisha <prishakhichadi@gmail.com>
2026-06-10 11:18:50 +0530

    createnewplayer fn

--- internal/models/player.go
+++ internal/models/player.go
@@ -21,3 +21,66 @@ type PlayerStats struct {
 	WinsDefense int    `db:"wins_defense" json:"wins_defense"`
 	TrophyCount int    `db:"trophy_count" json:"trophy_count"`
 }
+
+
+func CreateNewPlayer(db *sqlx.DB, username, passwordHash string) (string, error) {
+	tx, err := db.Beginx()
+	if err != nil {
+		return "", err
+	}
+	defer tx.Rollback()
+
+	pID := uuid.New().String()
+
+	//login innfo for players
+	_, err = tx.Exec(`
+		INSERT INTO players (id, username, password, created_at, updated_at)
+		VALUES ($1, $2, $3, NOW(), NOW())`, 
+		pID, username, passwordHash)
+	if err != nil {
+		return "", err
+	}
+
+	//info for player_stats
+	_, err = tx.Exec(`
+		INSERT INTO player_stats (player_id, wins_attack, wins_defense, trophy_count)
+		VALUES ($1, 0, 0, 0)`, 
+		pID)
+	if err != nil {
+		return "", err
+	}
+
+	//lvl 1 town tracker 
+	townID := uuid.New().String()
+	_, err = tx.Exec(`INSERT INTO town (id, player_id, level, created_at) VALUES ($1, $2, 1, NOW())`, townID, pID)
+	if err != nil {
+		return "", err
+	}
+
+	//init base economy resource balances
+	_, err = tx.Exec(`
+		INSERT INTO resources (id, player_id, gold, elixir, updated_at)
+		VALUES ($1, $2, 10000, 10000, NOW())`, 
+		uuid.New().String(), pID)
+	if err != nil {
+		return "", err
+	}
+
+	//static lvl 1 town hall identifier
+	var thInfoID string
+	err = tx.Get(&thInfoID, "SELECT id FROM building_info WHERE name = $1 AND town_level = 1 LIMIT 1", "Town Hall")
+	if err != nil {
+		return "", err
+	}
+
+	//deploy physical lvl 1 town hall
+	_, err = tx.Exec(`
+		INSERT INTO town_buildings (id, town_id, building_info_id, level, x, y)
+		VALUES ($1, $2, $3, 1, 20, 20)`, 
+		uuid.New().String(), townID, thInfoID)
+	if err != nil {
+		return "", err
+	}
+
+	return pID, tx.Commit()
+}
