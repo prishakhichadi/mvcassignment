f631e2b7e95e7655343adca1386f2eb5d034383b
prisha <prishakhichadi@gmail.com>
2026-06-20 01:13:30 +0530

    player profile handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,26 +15,11 @@ func main() {
 	auth := &controllers.AuthHandler{DB: dbConn}
 	town := &controllers.TownController{DB: dbConn}
 	troop := &controllers.TroopController{DB: dbConn}
-	/*
-		// auth
-		http.HandleFunc("/register", auth.Register)
-		http.HandleFunc("/login", auth.Login)
-
-		// town
-		http.HandleFunc("/town/layout", controllers.ContentGuard(town.GetLayout))
-		http.HandleFunc("/town/place", controllers.ContentGuard(town.PlaceStructure))
-
-		// troops
-		http.HandleFunc("/troop/train", controllers.ContentGuard(troop.TrainUnitsInstant))
-		http.HandleFunc("/troop/attack", controllers.ContentGuard(controllers.ExecuteRaid(dbConn)))
-
-		// leaderboard + replay
-		http.HandleFunc("/leaderboard", controllers.GetLeaderboard(dbConn))
-		http.HandleFunc("/battle/replay", controllers.ContentGuard(controllers.GetBattleReplay(dbConn)))
-	*/
+	player := &controllers.PlayerController{DB: dbConn} // NEW: backs /player/profile
 
 	http.HandleFunc("/register", controllers.CORSMiddleware(auth.Register))
 	http.HandleFunc("/login", controllers.CORSMiddleware(auth.Login))
+	http.HandleFunc("/player/profile", controllers.CORSMiddleware(controllers.ContentGuard(player.GetProfile))) // NEW
 	http.HandleFunc("/town/layout", controllers.CORSMiddleware(controllers.ContentGuard(town.GetLayout)))
 	http.HandleFunc("/town/place", controllers.CORSMiddleware(controllers.ContentGuard(town.PlaceStructure)))
 	http.HandleFunc("/troop/train", controllers.CORSMiddleware(controllers.ContentGuard(troop.TrainUnitsInstant)))
